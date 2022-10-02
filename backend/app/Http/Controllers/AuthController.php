6f74e0802c90c0b72eac7a3f8d9f560f9a28a6e5
unknown <aliiobeid01@gmail.com>
2022-10-02 13:01:07 +0300

    backend:implement the login function

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,8 +7,23 @@ use Validator;
 
 class AuthController extends Controller
 {
+    //login function with validator on the input user
+    //return the created token
+    public function login(Request $request){
 
-
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Please fill all fields'], 401);
+        }
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['message' => 'Incorrect email or pasword'], 401);
+        }
+        
+        return $this->respondWithToken($token);
+    }
 
 
 }
