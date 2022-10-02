fef13185849189ef2cd70cc8e5da10a2109e265c
unknown <aliiobeid01@gmail.com>
2022-10-02 13:05:52 +0300

    backend:implement created token function

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -57,4 +57,15 @@ class AuthController extends Controller
         return response()->json(['message' => 'User successfully signed out']);
     }
 
+    //function to return the user with the created token
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user'=>auth()->user()
+        ]);
+    }
+
 }
