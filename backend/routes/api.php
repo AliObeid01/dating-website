eee09a67127a9570071ee24ec12eb9885e88d17f
unknown <aliiobeid01@gmail.com>
2022-10-02 17:38:23 +0300

    backend:add the apis under the group route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\landingController;
 Route::group(["prefix"=> "v1"], function(){
 
 
-
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
+    
 });
