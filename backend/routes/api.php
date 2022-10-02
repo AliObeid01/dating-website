b92c57ffdbcb00ff79e225455368aa95bc03d097
unknown <aliiobeid01@gmail.com>
2022-10-02 17:39:30 +0300

    backend:add the apis under the midleware route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,19 @@ use App\Http\Controllers\landingController;
 Route::group(["prefix"=> "v1"], function(){
 
     Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('/user-profile', [landingController::class, 'userProfile']);
+        Route::get('/feed', [landingController::class, 'userFeed']);
+        Route::post('/update-profile', [landingController::class, 'updateProfile']);
+        Route::post('/favorite-user', [landingController::class, 'favoriteUser']);
+        Route::post('/unfavorite-user', [landingController::class, 'UnfavoriteUser']);
+        Route::get('/favorites', [landingController::class, 'getfavorites']);
+        Route::get('/favoriteby', [landingController::class, 'getFavoriteBy']);
+        Route::post('/block-user', [landingController::class, 'blockUser']);
+        Route::post('/send-message', [landingController::class, 'sendMessage']);
+        Route::get('/user-SendMessages', [landingController::class, 'getSentMessages']);
+        Route::get('/user-RecievedMessages', [landingController::class, 'getRecievedMessages']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
