8240499667c53030664ba1bd4c87bc50d5478ef0
unknown <aliiobeid01@gmail.com>
2022-10-04 20:03:02 +0300

    linking:fetch get recieved messages api

--- frontend/scripts/page_scripts.js
+++ frontend/scripts/page_scripts.js
@@ -193,3 +193,26 @@ workshop_pages.getAPI_MessagesSent = async () => {
         workshop_pages.Console("Error from GET API",  error);
     }
 }
+
+//fetch get recieved messages api
+workshop_pages.getAPI_MessagesRecieved = async () => {
+    let token = localStorage.getItem("token");
+    const Recieved_url = `${workshop_pages.baseURL}/user-RecievedMessages`;
+    try{
+        return await axios.get(
+            Recieved_url,
+            { headers:{
+                     "Authorization" : "Bearer " + token
+                }
+            }
+        ).then(function (response){
+        let recieved=document.getElementById('recieved');
+        for (let i = 0; i < response.data.length; i++) {
+            recieved.innerHTML+=`
+            <p>${response.data[i].message}<p>`
+        }
+        });
+    }catch(error){
+        workshop_pages.Console("Error from GET API",  error);
+    }
+}
